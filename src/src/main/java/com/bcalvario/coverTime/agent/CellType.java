2963376c66193d8f2e32b4572289f76bc26cbd77
Brandosuba <dinagreen32@gmail.com>
2025-06-16 17:47:50 -0700

    Completed AgentMain (beginning point) and begun on Environment and EnvironmentPanel.

--- src/src/main/java/com/bcalvario/coverTime/agent/CellType.java
+++ src/src/main/java/com/bcalvario/coverTime/agent/CellType.java
@@ -4,4 +4,3 @@ public enum CellType {
         ROAD,
         FOREST
     }
-}
--- src/src/main/java/com/bcalvario/coverTime/agent/Environment.java
+++ src/src/main/java/com/bcalvario/coverTime/agent/Environment.java
@@ -1,5 +1,40 @@
 package com.bcalvario.coverTime.agent;
 
-public class Environment {
+import org.jgrapht.Graph;
+import org.jgrapht.graph.DefaultEdge;
 
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+public class Environment {
+//    private final GridCell[][] grid;
+//    private final List<CarAgent> agents;
+//    private final Map<Integer, Point> cityLocations;
+
+    public Environment(int width, int height, Graph<Integer, DefaultEdge>graph, int numAgents) {
+//        this.grid = new GridCell[width][height];
+//        for(int x = 0; x < width; x++) {
+//            for(int y = 0; y < height; y++) {
+//                this.grid[x][y] = new GridCell(CellType.FOREST);
+//            }
+//        }
+//        this.agents = new ArrayList<>();
+//        List<Point> startingCities = new ArrayList<>(cityLocations.values());
+//        for(int x = 0; x < numAgents; x++) {
+//            Point startPos = startingCities.get(x % startingCities.size());
+//            this.agents.add(new CarAgent(startPos));
+//        }
+//        this.cityLocations = generateCityLocations(width,height,graph.vertexSet().size());
+//        cityLocations.forEach((cityId,point)->{ grid[point.x][point.y] = new GridCell(CellType.CITY)})
+    }
+    private Map<Integer,Point> generateCityLocations(int width, int height, int numCities){
+        return Map.of();
+    }
+//    public boolean allVisited(){
+//        Set<Point> visited = new HashSet<>();
+//
+//    }
+    public void step(){
+    }
 }
